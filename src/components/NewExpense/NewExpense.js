ab6c501793392206331a4a1a97a8d0f731a4b77b
CVOvidiu <cvovidiu2002@gmail.com>
2022-11-25 16:32:22 +0200

    Two-Way Binding: Reset the form fields upon submission

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -26,6 +26,12 @@ function NewExpense() {
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
+        console.log(expenseData);
+
+        // After submission, reset the input fields to empty
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -34,15 +40,15 @@ function NewExpense() {
                 <div className="new-expense__controls">
                     <div className="new-expense__control">
                         <label>Title</label>
-                        <input type="text" onChange={titleChangeHandler} />
+                        <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                     </div>
                     <div className="new-expense__control">
                         <label>Amount</label>
-                        <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                        <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                     </div>
                     <div className="new-expense__control">
                         <label>Date</label>
-                        <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                        <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
                     </div>
                 </div>
                 <div className="new-expense__actions">
