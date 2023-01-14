5d66dd74543d0b2fdde63b994097cbbbf340d232
CVOvidiu <cvovidiu2002@gmail.com>
2023-01-14 16:26:34 +0200

    useEffect: Preserve log in state

--- Authentication Project/src/App.js
+++ Authentication Project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,13 +7,34 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // const loggedInInfo = localStorage.getItem("isLoggedIn");
+  //
+  // This would create an infinite loop
+  // if(loggedInInfo === 'true') {
+  //   setIsLoggedIn(true); // *
+  // }
+
+  // This would only run once, when the App starts because the dependencies do not change because we do not have dependencies
+  useEffect(() => {
+    const loggedInInfo = localStorage.getItem("isLoggedIn");
+
+    if (loggedInInfo === "true") {
+      setIsLoggedIn(true); // Will reexecute but will not go through useEffect anymore because the deps didn't change
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem("isLoggedIn", "true");
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+
     setIsLoggedIn(false);
   };
 
